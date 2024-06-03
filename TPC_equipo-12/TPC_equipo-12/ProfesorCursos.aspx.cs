06324c96b0b4e9d7603afa655d53e7babc84b24a
Tomas Caceres <tomas.caceres2@alumnos.frgp.utn.edu.ar>
2024-06-03 02:09:01 -0300

    Webform Cursos de Profesor, funcionando. Refactorizacion de etiquetas navbar.

---
+++ TPC_equipo-12/TPC_equipo-12/ProfesorCursos.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
+
+namespace TPC_equipo_12
+{
+    public partial class ProfesorCursos : System.Web.UI.Page
+    {
+        public List<Curso> listaCursos = new List<Curso>();
+        public CursoNegocio cursoNegocio = new CursoNegocio();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                listaCursos = cursoNegocio.ListarCursos();
+                Session.Add("listaCursos", listaCursos);
+                rptProfesorCursos.DataSource = listaCursos;
+                rptProfesorCursos.DataBind();
+            }
+        }
+    }
+}
