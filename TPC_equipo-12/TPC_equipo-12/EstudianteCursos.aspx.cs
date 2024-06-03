10440094f28051aca123bd6aafa32df3e02722ac
Diegonzalez Dev <diegonzalezdev@gmail.com>
2024-06-03 20:03:21 -0300

    refactorizacion de metodo para traer los cursos en los que un estudiante esta inscripto, solo consultamos id y trabajamos con lista de cursos en session, ahora diferenciamos x idEstudiante

--- TPC_equipo-12/TPC_equipo-12/EstudianteCursos.aspx.cs
+++ TPC_equipo-12/TPC_equipo-12/EstudianteCursos.aspx.cs
@@ -11,17 +11,37 @@ namespace TPC_equipo_12
 {
     public partial class EstudianteCursos : System.Web.UI.Page
     {
-        public List<Curso> listaCursosInscriptos = new List<Curso>();
+        public List<Curso> listaCursos = new List<Curso>();
         public CursoNegocio cursoNegocio = new CursoNegocio();
+        public Estudiante EstudianteLogeado = new Estudiante();
+        public List<Curso> listaCursosInscriptos = new List<Curso>();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                listaCursosInscriptos = cursoNegocio.EstaInscripto();
-                Session.Add("listaCursos", listaCursosInscriptos);
+                listaCursos = cursoNegocio.ListarCursos();
+                EstaInscripto();
+                Session.Add("listaCursosInscriptos", listaCursosInscriptos);
                 rptCursos.DataSource = listaCursosInscriptos;
                 rptCursos.DataBind();
             }
+
+        }
+
+        protected void EstaInscripto()
+        {
+            List<int> auxIds= cursoNegocio.IDCursosXEstudiante(1);
+            foreach(int id in auxIds)
+            {
+                foreach(Curso curso in listaCursos)
+                {
+                    if(curso.IDCurso==id)
+                    {
+                        listaCursosInscriptos.Add(curso);
+                    }
+                }
+            }
+
 
         }
     }
