fad0d6fa9b13cf1a9ed51c5f9d51c4a909427064
Sebas Scollo <sebas.scollo24@gmail.com>
2024-06-20 23:02:19 -0300

    Agrega las clases Comentario y ComentarioNegocios Se agregan las siguientes clases para empezar a desarrollar la seccion'Preguntas y Respuestas' ubicada en EstudianteMateriales.aspx

---
+++ TPC_equipo-12/Dominio/Comentario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Comentario
+    {
+        int IDComentario {  get; set; }
+        int? IDComentarioPadre { get; set; }
+        Usuario UsuarioEmisor { get; set; }
+        string TituloComentario { get; set; }
+        string CuerpoComentario { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public bool Estado { get; set; }
+        int IDLeccion { get; set; }
+        public List<Comentario> Respuestas { get; set; } = new List<Comentario>();
+
+        public Comentario()
+        {
+            //FechaCreacion = DateTime.Now;
+            //Estado = true; 
+        }
+
+        public Comentario(string tituloComentario, string cuerpoComentario, int IdLeccion, Usuario usuarioEmisor, DateTime fechaCreacion, int? idComentarioPadre = null)
+        {
+            IDComentarioPadre = idComentarioPadre;
+            TituloComentario = tituloComentario;
+            CuerpoComentario = cuerpoComentario;
+            UsuarioEmisor = usuarioEmisor;
+            FechaCreacion = DateTime.Now;
+            IDLeccion = IdLeccion;
+            Estado = true; 
+        }
+
+    }
+}
---
+++ TPC_equipo-12/Negocio/ComentarioNegocio.cs
@@ -0,0 +1,43 @@
+﻿using AccesoDB;
+using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class ComentarioNegocio
+    {
+        private Datos datos;
+
+        public ComentarioNegocio() 
+        {
+            datos = new Datos();
+        }
+
+        public void publicarComentario(Usuario emisor, string tituloComentario, int idLeccion,  string comentario)
+        {
+            try
+            {
+                Comentario primerComentario = new Comentario(tituloComentario, comentario, idLeccion, emisor);
+                datos.SetearConsulta("INSERT INTO Comentarios(IDLeccion, TituloComentario, CuerpoComentario, IDUsuarioEmisor, FechaCreacion, Estado) VALUES (@iDLeccion, @tituloComentario, @cuerpoComentario, @idEmisor, @fechaCreacion, @estado");
+                //@iDLeccion, @tituloComentario, @cuerpoComentario, @idEmisor, @fechaCreacion, @estado
+                datos.SetearParametro("@iDleccion", primerComentario.IDLeccion);
+                datos.SetearParametro("@tituloComentario", primerComentario.TituloComentario);
+                datos.SetearParametro("@cuerpoComentario", primerComentario.CuerpoComentario);
+                datos.SetearParametro("@idEmisor", primerComentario.UsuarioEmisor.IDUsuario);
+                datos.SetearParametro("@fechaCreacion", primerComentario.FechaCreacion);
+                datos.SetearParametro("@estado", primerComentario.Estado);
+                datos.EjecutarAccion();
+                datos.LimpiarParametros();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
