3004b79603868f938d54d6ec04329e4382b6086b
Sebas Scollo <sebas.scollo24@gmail.com>
2024-07-08 13:28:10 -0300

    Se añade la en el dropdown del navar profesor un panel para camiar contrasenia
    Falta hacerlo en estudiante.

---
+++ TPC_equipo-12/TPC_equipo-12/Profesor/ProfesorCambiarContrasenia.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Profesor/ProfesorMasterPage.Master" AutoEventWireup="true" CodeBehind="ProfesorCambiarContrasenia.aspx.cs" Inherits="TPC_equipo_12.ProfesorCambiarContrasenia" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container">
+        <h2 class="text-center">Cambiar Contraseña</h2>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <asp:Label runat="server" Text="Contraseña Actual"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtContraseniaActual" TextMode="Password" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="mt-3">
+                    <asp:Label runat="server" ID="lblError" Text="La contraseña actual es incorrecta." CssClass="text-danger" Visible="false"></asp:Label>
+                </div>
+                <div class="mb-3">
+                    <asp:Label runat="server" Text="Nueva Contraseña"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtNuevaContrasenia" TextMode="Password" CssClass="form-control"></asp:TextBox>
+                </div>
+                <div class="mb-3">
+                    <asp:Label runat="server" Text="Confirmar Nueva Contraseña"></asp:Label>
+                    <asp:TextBox runat="server" ID="txtConfirmarContrasenia" TextMode="Password" CssClass="form-control"></asp:TextBox>
+                    <asp:CompareValidator runat="server" ControlToValidate="txtConfirmarContrasenia" ControlToCompare="txtNuevaContrasenia" ErrorMessage="Las contraseñas no coinciden." CssClass="text-danger"></asp:CompareValidator>
+                </div>
+                <asp:Button runat="server" ID="btnActualizarContrasena" Text="Actualizar Contraseña" OnClick="btnActualizarContrasena_Click" CssClass="btn btn-primary" />
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ TPC_equipo-12/TPC_equipo-12/Profesor/ProfesorCambiarContrasenia.aspx.cs
@@ -0,0 +1,72 @@
+﻿using Dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPC_equipo_12
+{
+    public partial class ProfesorCambiarContrasenia : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["profesor"] == null)
+            {
+                Session["MensajeError"] = "No puede acceder a esa pestaña sin ser profesor.";
+                Response.Redirect("../LogIn.aspx");
+            }
+
+            if (!IsPostBack)
+            {
+
+            }
+        }
+
+        protected void btnActualizarContrasena_Click(object sender, EventArgs e)
+        {
+            UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+            Usuario usuario = new Usuario();
+            usuario = (Usuario)Session["profesor"];
+            string contraseniaActual = txtContraseniaActual.Text;
+            string contraseniaNueva = txtNuevaContrasenia.Text;
+            if (!ValidarFormulario())
+            {
+                return;
+            }
+            if (usuarioNegocio.Logueo(usuario, contraseniaActual))
+            {
+                if (usuarioNegocio.CambiarContraseñaEnBaseDeDatos(usuario.IDUsuario, contraseniaNueva))
+                {
+
+                    Session["MensajeExito"] = "¡Contraseña cambiada con éxito!";
+                    Response.Redirect("DefaultProfesor.aspx");
+                }
+                else
+                {
+                    Session["MensajeError"] = "Error al cambiar la contraseña!";
+                    Response.Redirect("DefaultProfesor.aspx");
+                }
+            }
+            else
+            {
+                lblError.Visible = true;
+                return;
+            }
+            
+
+        }
+        protected bool ValidarFormulario()
+        {
+            if (txtContraseniaActual.Text == "" || txtNuevaContrasenia.Text == "" || txtConfirmarContrasenia.Text == "")
+            {
+                ScriptManager.RegisterStartupScript(this, typeof(Page), "info", "<script>showMessage('Faltan campos por completar!', 'info');</script>", false);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
